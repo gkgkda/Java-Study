58737d5c6c6d0ce72772e7172cbdbff21374c792
gkgkda <gkgkda00@gmail.com>
2023-03-29 17:24:00 +0900

    [Java] Chapter 09 : abstractClass

--- src/chap_09/_Quiz_09.java
+++ src/chap_09/_Quiz_09.java
@@ -4,7 +4,7 @@ import chap_09.camera.SpeedCam;
 import chap_09.detector.AccidentDetector;
 import chap_09.reporter.VideoReporter;
 
-public class _Quiz_08 {
+public class _Quiz_09 {
     public static void main(String[] args) {
         SpeedCam speedCam = new SpeedCam();
         speedCam.setDetector(new AccidentDetector());
